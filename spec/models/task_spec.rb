ecb17153132e00b5b85f7e465ccd4a3370c4d588
watsumi <w.utsumi2@gmail.com>
2020-12-14 22:46:46 +0900

    Fix:FactoryBotの設定

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  #pending "add some examples to (or delete) #{__FILE__}"
   describe 'validation' do
     it 'is invalid without a title' do 
       task = FactoryBot.build(:task, title: nil)
